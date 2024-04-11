3f6e1d1314b2f58bfa278ae07becc2fb5923f13b
Yue1Ran <s160418002@student.ubaya.ac.id>
2024-04-11 14:56:48 +0700

    update

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,6 +9,6 @@ buildscript {
 }
 22
 plugins {
-    id("com.android.application") version "8.1.1" apply false
+    id("com.android.application") version "8.3.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
 }
